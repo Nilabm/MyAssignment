2eb9a25b051435550aec9a807cb289cd73636dd0
Nilabm <109742495+Nilabm@users.noreply.github.com>
2022-08-03 21:54:21 +0530

    Add files via upload

---
+++ week1day2assignment/NumberIsPostiveOrNegative.java
@@ -0,0 +1,16 @@
+package week1day2assignment;
+
+public class NumberIsPostiveOrNegative {
+public static void main(String[] args) {
+	int number=35;
+	if(number>0) {
+		System.out.println("number is postive" +number);
+	}
+	else if (number<0) {
+		System.out.println("number is negative" +number);
+	}
+	else {
+		System.out.println("number is neither positive or negative");
+	}
+}
+}
